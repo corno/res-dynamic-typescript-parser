6848662db14aa37a876b258ecfcca6ca5f949d20
corno <corno.astn.a@schraverus.com>
2022-08-25 15:26:13 +0200

    .

--- pareto/scripts/clean.sh
+++ pareto/scripts/clean.sh
@@ -3,49 +3,9 @@
 scriptDir=`realpath $(dirname "$0")`
 rootDir="$scriptDir/../.."
 
-#make sure everything is pushed
-git push && \
-
-#validate that everything is committed and pushed (to make sure we're not messing with open work with updatePackage)
-git diff --exit-code && git log origin/master..master --exit-code && \
-
-#make sure latest buildenvironment is installed
-"$scriptDir/update2latestBuildEnvironment.sh" && \
-
-#delete the node_modules
-rm -rf $rootDir/pub/node_modules && \
+rm -rf $rootDir/dev/dist && \
 rm -rf $rootDir/dev/node_modules && \
-rm -rf $rootDir/test/node_modules && \
-
-#make sure latest packages are installed
-"$scriptDir/update2latestDependencies.sh" && \
-
-#buildAndTest
-"$scriptDir/buildAndTest.sh" && \
-
-#validate that everything is still committed after the update and build
-git diff --exit-code && \
-
-#bump version and store in variable
-pushd "$rootDir/pub" > /dev/null && \
-
-interfaceVersion=`npm pkg get interface-fingerprint`
-if [ $interfaceVersion == "{}" ]
-then
-    #no interface fingerprint
-
-    "$scriptDir/publishIfContentChanged.sh" "minor"
-
-else
-    name=$(npm pkg get name | cut -c2- | rev | cut -c2- |rev) 
-
-    localFingerprint=$(npm pkg get interface-fingerprint | cut -c2- | rev | cut -c2- |rev)
-    remoteFingerprint=$(npm view $name@latest interface-fingerprint)
-
-    if [ $localFingerprint != $remoteFingerprint ]
-    then
-        "$scriptDir/publishWithoutChecks.sh" "minor"
-    else
-        "$scriptDir/publishIfContentChanged.sh" "patch"
-    fi
-fi
+rm -rf $rootDir/pub/dist && \
+rm -rf $rootDir/pub/node_modules && \
+rm -rf $rootDir/test/dist && \
+rm -rf $rootDir/test/node_modules
